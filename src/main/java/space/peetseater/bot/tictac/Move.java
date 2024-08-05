b007e96f43a1a3b96daa7df966fa0f39c577ac22
Ethan Eldridge <ejayeldridge@gmail.com>
2024-08-04 22:39:30 -0400

    Return flag value for invalid Move

--- src/main/java/space/peetseater/bot/tictac/Move.java
+++ src/main/java/space/peetseater/bot/tictac/Move.java
@@ -8,10 +8,16 @@ public record Move(int x, int y, int value) {
 
     static final Pattern validMovePattern = Pattern.compile("([1-3])([1-3]) ([XxOo])");
 
+    public final static Move INVALID = new Move(-1, -1, -1);
+
     public static Move parseMove(String noTT) {
 
         Scanner scanner = new Scanner(noTT);
-        scanner.findInLine(validMovePattern);
+        String found = scanner.findInLine(validMovePattern);
+        if (found == null) {
+            return INVALID;
+        }
+
         MatchResult matchResult = scanner.match();
 
         String xString = matchResult.group(1);
@@ -20,19 +26,11 @@ public record Move(int x, int y, int value) {
 
         int x = Integer.parseInt(xString);
         int y = Integer.parseInt(yString);
-        int v;
-        switch (vString) {
-            case "X":
-            case "x":
-                v = TicTacGame.X;
-                break;
-            case "O":
-            case "o":
-                v = TicTacGame.O;
-                break;
-            default:
-                v = TicTacGame.EMPTY;
-        }
+        int v = switch (vString) {
+            case "X", "x" -> TicTacGame.X;
+            case "O", "o" -> TicTacGame.O;
+            default -> TicTacGame.EMPTY;
+        };
 
         return new Move(x, y, v);
     }
