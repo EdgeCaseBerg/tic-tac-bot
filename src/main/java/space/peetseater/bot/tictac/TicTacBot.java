0b6b4d7d1f7058488bffa31be47c6fc0bd5b38bf
Ethan Eldridge <ejayeldridge@gmail.com>
2024-08-05 01:31:37 -0400

    Refactor TicTacBot to use bot commands
    This was done in multiple small steps, changing the if (command.equals
    ("")) type strings into botCommand.canHandle with each sub type being
    used. Once that was complete, I noticed we needed to do the game over
    check in the middle of things, before the move or show command, so I
    made two command lists and added the loop.
    
    Plenty of the helper methods can now be removed since their
    functionality has moved into the various commands.
    
    As noted in a prior commit, I want to figure out a better way to wrap
    things with the ``` and not repeat that various places. I might just add
     it to the toString for the game and call it a day though.

--- src/main/java/space/peetseater/bot/tictac/TicTacBot.java
+++ src/main/java/space/peetseater/bot/tictac/TicTacBot.java
@@ -4,20 +4,31 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import space.peetseater.bot.tictac.commands.*;
 
-import java.util.regex.Matcher;
+import java.util.LinkedList;
 
 public class TicTacBot extends ListenerAdapter {
 
     TicTacGame ticTacGame;
+    LinkedList<BotCommand> anyTimeCommands;
+    LinkedList<BotCommand> gameInProgressCommands;
 
     public TicTacBot() {
         ticTacGame = new TicTacGame();
-    }
+        HelpCommand helpCommand = new HelpCommand();
+        ShowCommand showCommand = new ShowCommand(ticTacGame);
+        NewGameCommand newGameCommand = new NewGameCommand(ticTacGame);
+        MoveCommand moveCommand = new MoveCommand(ticTacGame);
 
-    public boolean isValidMoveInput(String raw) {
-        Matcher matcher = Move.validMovePattern.matcher(raw);
-        return matcher.matches();
+        anyTimeCommands = new LinkedList<>();
+        anyTimeCommands.add(helpCommand);
+        anyTimeCommands.add(newGameCommand);
+
+        gameInProgressCommands = new LinkedList<>();
+        gameInProgressCommands.add(moveCommand);
+        gameInProgressCommands.add(showCommand);
+
     }
 
     public void sendMessage(MessageChannel channel, String message) {
@@ -42,80 +53,38 @@ public class TicTacBot extends ListenerAdapter {
     }
 
     private void handleMessage(String command, MessageChannel channel) {
-        if (command.equals("show")) {
-            sendBoardStatus(channel);
-            return;
+        for (BotCommand botCommand : anyTimeCommands) {
+            if (botCommand.canHandle(command)) {
+                String response = botCommand.handle(command);
+                sendMessage(channel, response);
+                return;
+            }
         }
 
-        if (command.equals("new")) {
-            ticTacGame.reset();
-            sendMessage(channel, "New game started! Take your first move!");
-            return;
+        if (gameAlreadyOver(channel)) return;
+
+        for (BotCommand botCommand : gameInProgressCommands) {
+            if (botCommand.canHandle(command)) {
+                String response = botCommand.handle(command);
+                sendMessage(channel, response);
+                return;
+            }
         }
 
-        if (command.equals("help")) {
-            sendHelp(channel);
-            return;
-        }
+        sendMessage(channel, "I didn't understand your message, try `!tt help` for help");
+    }
 
+    private boolean gameAlreadyOver(MessageChannel channel) {
         if (ticTacGame.hasWinner()) {
             sendMessage(channel, "The game has already ended, use `!tt new` to start again");
-            return;
+            return true;
         }
 
         if (ticTacGame.isBoardFilled()) {
-            sendMessage(channel, "The board is full! use !tt new to start fresh");
-            return;
+            sendMessage(channel, "The board is full! use `!tt new` to start fresh");
+            return true;
         }
-
-        if (isValidMoveInput(command)) {
-            Move move = Move.parseMove(command);
-            if (!ticTacGame.canPlaceValue(move.x(), move.y(), move.value())) {
-                sendMessage(channel, "You can't place that there, try again\n" + getFormattedBoardString());
-                return;
-            }
-
-            ticTacGame.setBoxTo(move.x(), move.y(), move.value());
-
-            if (ticTacGame.hasWinner()) {
-                String winner = ticTacGame.getWinnerName();
-                sendMessage(channel,"%s has won, use `!tt new` to start a new game\n%s".formatted(winner, getFormattedBoardString()));
-            } else {
-                sendBoardStatus(channel);
-            }
-
-            return;
-        }
-
-        sendMessage(channel, "I didn't understand your message, try !tt help");
-    }
-
-    private void sendHelp(MessageChannel channel) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Use `!tt` to send commands");
-        stringBuilder.append("\n");
-        stringBuilder.append("\t `!tt show` Will show you the current board");
-        stringBuilder.append("\n");
-        stringBuilder.append("\t `!tt new` Will reset the board to a clean slate");
-        stringBuilder.append("\n");
-        stringBuilder.append("\t `!tt help` Will show this text");
-        stringBuilder.append("\n");
-        stringBuilder.append("\t `!tt 12 X`\t Will set the first row and second column value to X");
-        stringBuilder.append(", ");
-        stringBuilder.append("you may use numbers `1`, `2`, and `3` to address rows and columns");
-        stringBuilder.append(", ");
-        stringBuilder.append("you may use `X`,`x`,`O`, or `o` for the values to set on the board");
-        sendMessage(channel, stringBuilder.toString());
-    }
-
-    private void sendBoardStatus(MessageChannel channel) {
-        sendMessage(channel, getFormattedBoardString());
-    }
-
-    private String getFormattedBoardString() {
-        String currentBoard = ticTacGame.boardString();
-        String codeTag = "```";
-        return codeTag + currentBoard + codeTag;
+        return false;
     }
 }
 
